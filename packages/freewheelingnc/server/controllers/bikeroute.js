2688dbda723091a29c6c1041a3bd02046a4fe6ab
Anthony Castrati <anthony.castrati@eclinicalos.com>
2014-07-26 01:18:23 -0400

    Started work on a page that will display page route JSON

---
+++ packages/freewheelingnc/server/controllers/bikeroute.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  BikeRoute = mongoose.model('BikeRoute');
+
+/**
+ * List of Bike Routes
+ */
+exports.all = function(req, res) {
+  BikeRoute.find().exec(function(err, bikeRoutes) {
+    if (err) {
+      return res.json(500, {
+        error: 'Cannot list the routes'
+      });
+    }
+    res.json(bikeRoutes);
+
+  });
+};
